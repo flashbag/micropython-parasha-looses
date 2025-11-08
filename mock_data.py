9274508efeff0f459b35d06cdc46a1a9ec6c377e
Oleh Kukil <oleggg.normal@gmail.com>
2025-11-08 12:38:37 +0200

    Update mock HTML content with new dates and data

--- mock_data.py
+++ mock_data.py
@@ -1,39 +1,57 @@
 # mock_data.py
 
 MOCK_HTML_CONTENT = """
-<div class="container_news_body" style="height: auto !important;">
-    <article class="post" style="height: auto !important;">
+<div class="container_news_body">
+    <article class="post">
         <header class="post_header">
-            <h1 style="padding-top: 5px" class="post_title">Втрати ворога станом на 2 листопада 2025 – Генштаб ЗСУ</h1>
-        
+            <h1 style="padding-top: 5px" class="post_title">Втрати ворога станом на 7 листопада 2025 – Генштаб ЗСУ</h1>
             <div class="post_info_wrap 1">
                 <div class="post_info">
+                    <a class="post_info_author" href="/authors/rostyslav_vons.html">Ростислав Вонс</a>
                     <div>
-                        <time class="article_date" datetime="2025-11-02T06:36:00+02:00">2 листопада, 2025, 06:36</time>
+                        <time class="article_date" datetime="2025-11-07T07:02:00+02:00">7 листопада, 2025, 07:02</time>
+                        <div><span style="padding-right: 5px" data-edit="1087165,6709" data-absnum="1087165">  glavcom.ua</span></div>
                     </div>
+                    <a class="post_info_author_mob" href="/authors/rostyslav_vons.html">Ростислав Вонс</a>
                 </div>
+
             </div>
         </header>
-        <div style="padding-top: 15px; height: auto !important;" class="post_content">
-            <h3>Втрати Росії у війні на 2 листопада 2025 року</h3>
-            <p>РФ у війні з Україною станом на 2 листопада втратила:</p>
-            <ul>
-                <li>особового складу – близько 1 143 670 (+940) осіб;</li>
-                <li>танків – 11 316 (+0) од;</li>
-                <li>бойових броньованих машин – 23 525 (+4) од;</li>
-                <li>артилерійських систем – 34 162 (+25) од;</li>
-                <li>РСЗВ – 1 534 (+0) од;</li>
-                <li>засоби ППО – 1 235 (+0) од;</li>
-                <li>літаків – 428 (+0) од;</li>
-                <li>гелікоптерів – 346 (+0) од;</li>
-                <li>БпЛА оперативно-тактичного рівня – 77 052 (+348) од;</li>
-                <li>крилаті ракети – 3 917 (+0) од;</li>
-                <li>кораблі / катери – 28 (+0) од;
-                <li>підводні човни – 1 (+0) од;</li>
-                <li>автомобільна техніка та автоцистерни – 66 290 (+121) од;</li>
-                <li>спеціальна техніка – 3 987 (+0) од.</li>
-            </ul>
+        <div style="padding-top: 15px" class="post_content">
+            <aside class="post_side">
+            </aside>
+
+            <h2 class="post_subtitle">
+                <p>Протягом минулої доби українські захисники знищили 1 170 окупантів</p>
+            </h2>
+
+            <div class="post_text art_body_uniq">
+                <p><a href="/tags/genshtab.html">Генеральний штаб Збройних сил України</a> оновив втрати російських окупантів станом на ранок пʼятниці, 7 листопада 2025 року.</p>
+
+                <h3>Втрати Росії у війні на 7 листопада 2025 року</h3>
+                <p>РФ у війні з Україною станом на 7 листопада <a href="https://www.facebook.com/share/p/17YpeC5BNb/">втратила</a>:</p>
+                <ul>
+                    <li>особового складу – близько 1 148 910 (+1 170) осіб</li>
+                    <li>танків – 11 330 (+1) од.</li>
+                    <li>бойових броньованих машин – 23 543 (+2) од.</li>
+                    <li>артилерійських систем – 34 301 (+13) од.</li>
+                    <li>РСЗВ – 1 535 (+0) од.</li>
+                    <li>засоби ППО – 1 238 (+1) од.</li>
+                    <li>літаків – 428 (+0) од.</li>
+                    <li>гелікоптерів – 346 (+0) од.</li>
+                    <li>БпЛА оперативно-тактичного рівня – 78 678 (+248) од.</li>
+                    <li>крилаті ракети – 3 918 (+0) од.</li>
+                    <li>кораблі / катери – 28 (+0) од.</li>
+                    <li>підводні човни – 1 (+0) од.</li>
+                    <li>автомобільна техніка та автоцистерни – 66 723 (+65) од.</li>
+                    <li>спеціальна техніка – 3 993 (+2) од.</li>
+                </ul>
+
+
+            </div>
         </div>
     </article>
 </div>
 """
+
+
